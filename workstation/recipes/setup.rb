d6f816dce993f8ce0a65a65ad23f985a34543e77
sekharakkisetti <sekhar.akkisetti@gmail.com>
2018-08-01 01:13:02 +0000

    Release version 0.2.0

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -3,5 +3,18 @@ package 'tree' do
 end
 
 file '/etc/motd' do
-  content 'Property of...'
+  #content 'Property of...
+
+  #IPADDRESS : 172.31.27.76
+  #HOSTNAME : ip-172-31-27-76
+  #MEMORY : 546156 kB
+  #CPU : 1795.673 MHz
+#,
+  content "Property of ...
+  IPADDRESS : #{node['ipaddress']}
+  HOSTNAME : #{node['hostname']}
+  MEMORY : #{node['memory']['total']}
+  CPU : #{node['cpu']['0']['mhz']}
+"
+
 end
