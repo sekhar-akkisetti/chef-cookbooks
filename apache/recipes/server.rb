6678fe5363b7a92cf8cbd5e8123794d859f6b9f8
sekharakkisetti <sekhar.akkisetti@gmail.com>
2018-08-01 01:21:57 +0000

    Release version 0.2.0

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -4,7 +4,11 @@ package 'httpd'
 #end
 
 file '/var/www/html/index.html' do
-  content '<h1> Hello Rakuten !!!<h1>'
+  content "<h1> Hello Rakuten !!!<h1>
+  <h2> IP Adress : #{node['ipaddress']} </h2>
+  <h2> Host name : #{node['hostname']} </h2>
+  
+"
 end
 
 service 'httpd' do
