1aa2c51eee55faf29030443e2c87f49a34c3774f
sekhar-akkisetti <sekhar.akkisetti@gmail.com>
2018-08-01 06:29:55 +0000

    Updated dafault recipe to use template

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -2,7 +2,7 @@ package 'tree' do
   action :install
 end
 
-file '/etc/motd' do
+#file '/etc/motd' do
   #content 'Property of...
 
   #IPADDRESS : 172.31.27.76
@@ -10,11 +10,13 @@ file '/etc/motd' do
   #MEMORY : 546156 kB
   #CPU : 1795.673 MHz
 #,
-  content "Property of ...
-  IPADDRESS : #{node['ipaddress']}
-  HOSTNAME : #{node['hostname']}
-  MEMORY : #{node['memory']['total']}
-  CPU : #{node['cpu']['0']['mhz']}
-"
-
+#  content "Property of ...
+#  IPADDRESS : #{node['ipaddress']}
+#  HOSTNAME : #{node['hostname']}
+#  MEMORY : #{node['memory']['total']}
+#  CPU : #{node['cpu']['0']['mhz']}
+#"
+#end
+template '/etc/motd' do
+  source 'motd.erb'
 end
