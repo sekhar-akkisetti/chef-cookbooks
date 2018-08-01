1eaa4db00a18a09f87c014a46b71c58568619f97
sekhar-akkisetti <sekhar.akkisetti@gmail.com>
2018-08-01 06:12:55 +0000

    Release version 0.2.1

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -3,12 +3,14 @@ package 'httpd'
 #  action :install
 #end
 
-file '/var/www/html/index.html' do
-  content "<h1> Hello Rakuten !!!<h1>
-  <h2> IP Adress : #{node['ipaddress']} </h2>
-  <h2> Host name : #{node['hostname']} </h2>
-  
-"
+#file '/var/www/html/index.html' do
+#  content "<h1> Hello Rakuten !!!<h1>
+#  <h2> IP Adress : #{node['ipaddress']} </h2>
+#  <h2> Host name : #{node['hostname']} </h2> 
+#"
+#end
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
